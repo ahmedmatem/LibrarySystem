3cc04a0a301c162e33c3a2ea075d07de72e296a7
Ahmed Matem <ahmedmatem@gmail.com>
2025-06-27 19:39:08 +0300

    Create read and write functionality of the data

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,6 +1,33 @@
-﻿namespace LibrarySystem.Data
+﻿using LibrarySystem.Model;
+using System.Text.Json;
+
+namespace LibrarySystem.Data
 {
     public class DataContext
     {
+        private const string dataPath = "../../../data.txt";
+
+        private StreamWriter writer;
+        private StreamReader reader;
+
+        public List<Book> Books { get; set; }
+
+        public List<Book> GetAllBooks()
+        {
+            using(reader = new StreamReader(dataPath))
+            {
+                string data = reader.ReadToEnd();
+                return JsonSerializer.Deserialize<List<Book>>(data)!;
+            }
+        }
+
+        public void Save(List<Book> books)
+        {
+            using (writer = new StreamWriter(dataPath))
+            {
+                string data = JsonSerializer.Serialize(books);
+                writer.Write(data);
+            }
+        }
     }
 }
